beb6752dea8dc6122812776c372d45a705311cda
vamsiampolu <vamsiampolu3000@gmail.com>
2018-01-24 10:15:43 +1100

    Got traverse working

--- string-calculator/src/main/scala/example/StringCalculator.scala
+++ string-calculator/src/main/scala/example/StringCalculator.scala
@@ -1,7 +1,8 @@
 package example
 
-import java.lang.NumberFormatException
-import cats.syntax.traverse._
+// import java.lang.NumberFormatException
+// import cats.Semigroup
+import cats.implicits._
 
 object StringCalculator {
   def sumList(numList: List[String]): Int = {
@@ -10,22 +11,28 @@ object StringCalculator {
       .foldLeft(0)(_ + _)
   }
 
-  def add(numbers: String):Either[Int, Int] = {
+  def add(numbers: String) = {
     if (numbers == "") {
       Right(0)
     } else if(numbers.contains(',')) {
       val numList = numbers.split(',').toList
-      numList.traverse(x => {
+      val l2 = numList.traverse(x => {
           try {
               Right(x.toInt)
           } catch {
-            case e: NumberFormatException => Left(0)
+            case _: NumberFormatException => Left(0)
           }
         })
-        .fold(???)
+        .fold(x => x, x => x)
+      l2 match {
+        case Left(x) => Left(0)
+        case Right(xs:List[Int]) => {
+          Right(xs.foldLeft(0)(_ + _))
+        }
+      }
     } else if(numbers.contains('\n')) {
       val numList = numbers.split('\n').toList
-      numList
+      val l2 = numList
         .traverse(x => {
           try {
               Right(x.toInt)
@@ -33,7 +40,13 @@ object StringCalculator {
             case e: NumberFormatException => Left(0)
           }
         })
-        .fold(???)
+      l2 match {
+        case Left(x:Int) => Left(0)
+        case Right(xs: List[Int]) => {
+          println(xs.foldLeft(0)(_ + _))
+          Right(xs.foldLeft(0)(_ + _))
+        }
+      }
     } else {
       Right(numbers.toInt)
     }
